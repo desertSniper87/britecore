7d57cc405000ede120101c20755c17d7f6adf1b1
desertsniper87 <torshobuet@gmail.com>
2019-05-26 03:13:01 +0600

    nixos db setup

--- britecore/settings.py
+++ britecore/settings.py
@@ -73,12 +73,23 @@ WSGI_APPLICATION = 'britecore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(default=DATABASE_URL)
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('PG_DB', 'britecore'),
+            'USER': os.environ.get('PG_USER', 'britecore'),
+            'PASSWORD': os.environ.get('PG_PASSWORD', 'britecore'),
+            'HOST': os.environ.get('PG_HOST', '127.0.0.1'),
+            'PORT': 5432,
+        }
+    }
 
 
 # Password validation
--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env nix-shell
-#! nix-shell -i python3 -p "python3.withPackages(ps: [ps.django])"
+#! nix-shell -i python3 -p "python3.withPackages(ps: [ps.django_2_1 ps.psycopg2])"
 
 import os
 import sys
